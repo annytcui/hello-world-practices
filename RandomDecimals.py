7f859d816c91554904cb80abb66292ec30af8e21
Tianyuan Cui <tianyuan.cui.sc@gmail.com>
2017-08-02 16:54:14 +0800

    Add some small programs

---
+++ RandomDecimals.py
@@ -0,0 +1,6 @@
+import random, time
+
+print "The program starts 3 seconds later..."
+for i in range(10):
+    time.sleep(3)
+    print random.random()
---
+++ RandomIntegers.py
@@ -0,0 +1,8 @@
+import random
+
+List = []
+for i in range(5):
+    integer = random.randint(1, 20)
+    List.append(integer)
+
+print List
